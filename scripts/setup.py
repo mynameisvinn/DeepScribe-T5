8c8b531cddd782448c9c28fa4092cc26ecf7b3cb
Vin Tang <vin@Vins-MBP.home>
2021-03-09 10:50:02 -0500

    sagemaker

---
+++ scripts/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='DeepScribe AI',
+    version="1.0.0",
+    author='Vin Tang',
+    author_email='vin.tang@gmail.com',
+    description='T5',
+)
--- scripts/train.py
+++ scripts/train.py
@@ -1,23 +1,26 @@
-from model import create_model
+from t1000.metrics import evaluate
+from t1000.model import create_model
 
+import os
 import argparse
-import gc
-import traceback
-import torch
+
+from transformers import T5Tokenizer, T5ForConditionalGeneration,Adafactor
 import torch
 import pandas as pd
-from transformers import T5Tokenizer, T5ForConditionalGeneration,Adafactor
-import os
 import numpy as np
-from metrics import evaluate
+import logging
 
+LOG = logging.getLogger(__name__)
 
 def parser():
     p = argparse.ArgumentParser(description='Train T5 model')
+    p.add_argument('--model_dir', type=str, default=os.environ.get('SM_MODEL_DIR'))  # for pytorch
+    p.add_argument('--data_dir', type=str, default = os.environ.get('SM_CHANNEL_TRAINING'))
+    p.add_argument('--output_path', default = os.environ.get('SM_MODEL_DIR'))
     return p.parse_args()
 
 
-def train(model, optimizer, tokenizer, train_df, test_df, training_column, n_epochs, batch_size):
+def train(model, optimizer, tokenizer, train_df, test_df, training_column, n_epochs, batch_size, output_path):
     
     n_batches = int(len(train_df)/batch_size)
     model.train()
@@ -43,26 +46,39 @@ def train(model, optimizer, tokenizer, train_df, test_df, training_column, n_epo
             # forward pass
             optimizer.zero_grad()
             outputs = model(input_ids=inputbatch, labels=labelbatch)
-            loss = outputs.loss.item()
+            loss = outputs.loss
+            loss_val = loss.item()
             loss.backward()
             optimizer.step()
 
-            print(f'>> {epoch} train loss {loss}')
+            print(f'>> {epoch} train loss {loss_val}')
                 
     checkpoint = {'state_dict': model.state_dict(),'optimizer_state_dict': optimizer.state_dict()}
-    torch.save(checkpoint, f'model/checkpoint_{epoch}_{loss_num}.pt')
+    path = os.path.join(output_path, f'checkpoint_{epoch}_{loss_val}.pt')
+    torch.save(checkpoint, path)
 
       
 if __name__ == '__main__':
+    LOG.info('Initializing training module...')
+
     args = parser()
 
-    train_df = pd.read_csv('./data/train_df.csv')
-    test_df = pd.read_csv('./data/test_df.csv')
+    # train_df = pd.read_csv('./data/train_df.csv')
+    # test_df = pd.read_csv('./data/test_df.csv')
+    
+    data_dir = args.data_dir
+    train_df = pd.read_csv(os.path.join(data_dir, "train_df.csv"))
+    test_df = pd.read_csv(os.path.join(data_dir, "test_df.csv"))
+
     training_column = "cat_conc_sec"
+
 
     m, t, o = create_model()
 
     n_epochs = 1
     batch_size = 16
 
-    train(m, o, t, train_df, test_df, training_column, n_epochs, batch_size)
+    output_path = args.output_path
+
+    train(m, o, t, train_df, test_df, training_column, n_epochs, batch_size, output_path)
+    LOG.info('Train Done.')
